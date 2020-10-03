b5051ff7e150a6da1a9d78976abe97beab1b574f
Philip Zhang <jmslca123@gmail.com>
2020-10-03 13:42:32 -0700

    update readme, add some components

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text>Welcome to PuzzlePieces!!</Text>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        onPress={function () {
+          alert("clicked!");
+        }}
+        title="Cool button"
+        color="#841584"
+      />
     </View>
   );
 }
@@ -14,8 +22,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,46 @@
+# PuzzlePieces
+
+## Getting Started
+
+1. Install dependencies(using yarn or npm)
+
+   - `yarn install`
+
+     **OR**
+
+   - `npm install`
+
+2. Start Expo
+
+   - `yarn/npm start`
+
+     **OR**
+
+   - `expo start`
+
+## Run the app
+
+To run the app in:
+
+1. A web browser
+
+   - Click in "Run in web browser" in the sidebar of the Expo DevTools window
+   - The app will open in another tab
+
+2. On your smartphone
+
+   - Download the [Expo Client](https://expo.io/tools#client) app on your smartphone
+   - Scan the QR code in the Expo DevTools window or in the CLI with the camera app on your phone
+
+3. In an emulator
+   1. Android
+      - Click "Run on Android device/emulator"
+   2. iOS
+      - Click "Run on iOS simulator"
+      - This uses the Simulator app that comes with Xcode
+
+---
+
+## Frontend
+
+- Expo(React Native)
