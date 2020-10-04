d864f8ccb7e670c992aa1d6e660f1696cd69d3a9
Herrick Fang <herrickfang@gmail.com>
2020-10-04 14:26:42 -0700

    [infra] Add husky,lint-staged,prettier,eslint

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  quoteProps: 'consistent',
+  trailingComma: 'all',
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+};
