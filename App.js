b7757cd4d90c3a9f7060f8034a8e2d86d05475f9
Herrick Fang <herrickfang@gmail.com>
2020-10-04 15:04:54 -0700

    [devops] Add an eslint, prettier config on pre-commit (#5)
    * [infra] Add husky,lint-staged,prettier,eslint
    
    * add parser options ecma
    
    * add airbnb dependency
    
    * working?
    
    * clean
    
    * update config
    
    * package.json cleanup
    
    * clean up eslint

--- App.js
+++ App.js
@@ -1,29 +1,28 @@
-import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Welcome to PuzzlePieces!!</Text>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Button
-        onPress={function () {
-          alert("clicked!");
-        }}
-        title="Cool button"
-        color="#841584"
-      />
-    </View>
-  );
-}
+import {StatusBar} from 'expo-status-bar';
+import React from 'react';
+import {StyleSheet, Text, View, Button} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
+const alertNow = () => {
+  alert('clicked!');
+};
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Welcome to PuzzlePieces!!</Text>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar />
+      <Button onPress={alertNow} title='Cool button' color='#841584' />
+    </View>
+  );
+};
+
+export default App;
