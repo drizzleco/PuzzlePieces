ebe4da8c4f17b014477c306e067dd1ddfeddcf6a
Herrick Fang <herrickfang@gmail.com>
2020-10-24 14:08:51 -0700

    remove expo, go for create-react app

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import logo from './logo.svg';
+import './App.css';
+import * as firebase from 'firebase';
+import 'firebase/firestore';
+//import firebaseConfig from './firebaseConfig.js';
+
+//firebase.initializeApp(firebaseConfig);
+//
+//const dbh = firebase.firestore();
+//
+//const alertNow = () => {
+//  dbh.collection('characters').doc('luigi').set({
+//    employment: 'dude',
+//    outfitColor: 'green',
+//    specialAttack: 'fireball',
+//  });
+//};
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
