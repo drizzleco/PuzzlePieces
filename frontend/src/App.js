74c8ebafff9513243f85af01a0cd81a2b19fe02c
Philip Zhang <jmslca123@gmail.com>
2020-11-01 11:11:41 -0800

    add reach/router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import InitialApp from './components/InitialApp';
+import {Router} from '@reach/router';
 
 const App = () => {
-  return <InitialApp />;
+  return (
+    <Router style={{height: '100%'}}>
+      <InitialApp path='/' />
+    </Router>
+  );
 };
 
 export default App;
