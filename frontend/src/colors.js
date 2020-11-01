4aef23b23dee821b0c6d1e6e46973a46e2c71814
Philip Zhang <jmslca123@gmail.com>
2020-11-01 10:55:18 -0800

    cleanup + colors.js

---
+++ frontend/src/colors.js
@@ -0,0 +1,13 @@
+const colors = {
+  yellow2: '#FEFFD8',
+  yellow4: '#FFBF00',
+
+  orange1: '#FF9647',
+  purple3: '#875DA9',
+
+  white16: '#FFFFFF',
+
+  black16: '#000000',
+};
+
+export default colors;
