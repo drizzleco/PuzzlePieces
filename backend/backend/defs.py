1c7810f3e770d621c860e5d4c8f43ffab0b075ff
Herrick Fang <herrickfang@gmail.com>
2022-11-27 16:10:18 -0800

    change env variables?

--- backend/backend/defs.py
+++ backend/backend/defs.py
@@ -9,14 +9,14 @@ CREDENTIALS = credentials.Certificate(
     {
         "type": "service_account",
         "project_id": "puzzlepieces-25386",
-        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-        "private_key": os.getenv("FIREBASE_PRIVATE_KEY"),
-        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+        "private_key_id": os.getenv("RENDER_FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": os.getenv("RENDER_FIREBASE_PRIVATE_KEY"),
+        "client_email": os.getenv("RENDER_FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.getenv("RENDER_FIREBASE_CLIENT_ID"),
         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
         "token_uri": "https://oauth2.googleapis.com/token",
         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+        "client_x509_cert_url": os.getenv("RENDER_FIREBASE_CLIENT_CERT_URL"),
     }
 )
 
